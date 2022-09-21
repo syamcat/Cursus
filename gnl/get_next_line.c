b41a368aa7106ceea3766586204df3c26bc783da
Dohyeon Kim <dohyeoki@c6r10s6.42seoul.kr>
2022-09-21 17:35:33 +0900

    test_file(add)

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -14,5 +14,14 @@
 
 char	*get_next_line(int fd)
 {
-	
+	static char	buff[BUFFER_SIZE];	// 버퍼 선언
+	ssize_t		ck; 				// read 함수로 읽어온 바이트 수 저장할 변수 선언
+
+	ch = 1;
+	while (0 < ck) 					// 읽어온 바이트 수가 0이 될 때 까지 반복
+	{
+		ck = result = read(fd, buff, BUFFER_SIZE);	// read 함수 사용
+		if (ck == -1)				// read 함수 실패하면 NULL 반환
+			return (NULL);
+	}
 }
