9b587e72eb249c85eddfb5ac0789224a3bf66b80
Dohyeon Kim <dohyeoki@c3r9s4.42seoul.kr>
2022-12-13 19:44:00 +0900

    printf(ap_pointer->variable)

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,29 +6,29 @@
 /*   By: dohyeoki <dohyeoki@student@42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:09:24 by dohyeoki          #+#    #+#             */
-/*   Updated: 2022/12/12 18:46:27 by dohyeoki         ###   ########.fr       */
+/*   Updated: 2022/12/13 19:40:52 by dohyeoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #define INT_MAX 2147483647
 
-static int	ft_print_conv(va_list *ap, const char *org, int *i, int len)
+static int	ft_print_conv(va_list ap, const char *org, int *i, int len)
 {
 	if (org[*i + 1] == 'c')
-		len = ft_putchar_fd(va_arg(*ap, int), 1);
+		len = ft_putchar_fd(va_arg(ap, int), 1);
 	else if (org[*i + 1] == 's')
-		len = ft_putstr_fd(va_arg(*ap, char *), 1);
+		len = ft_putstr_fd(va_arg(ap, char *), 1);
 	else if (org[*i + 1] == 'p')
-		len = ft_prt_pnt(va_arg(*ap, t_ull), "0123456789abcdef");
+		len = ft_prt_pnt(va_arg(ap, t_ull), "0123456789abcdef");
 	else if (org[*i + 1] == 'd' || org[*i + 1] == 'i')
-		len = ft_putnbr_fd(va_arg(*ap, int), 1);
+		len = ft_putnbr_fd(va_arg(ap, int), 1);
 	else if (org[*i + 1] == 'u')
-		len = ft_putunbr_fd(va_arg(*ap, unsigned int), 1, 1);
+		len = ft_putunbr_fd(va_arg(ap, unsigned int), 1, 1);
 	else if (org[*i + 1] == 'x')
-		len = ft_putnbr_base(va_arg(*ap, unsigned int), "0123456789abcdef");
+		len = ft_putnbr_base(va_arg(ap, unsigned int), "0123456789abcdef");
 	else if (org[*i + 1] == 'X')
-		len = ft_putnbr_base(va_arg(*ap, unsigned int), "0123456789ABCDEF");
+		len = ft_putnbr_base(va_arg(ap, unsigned int), "0123456789ABCDEF");
 	else if (org[*i + 1] == '%')
 		len = write(1, "%", 1);
 	else
@@ -52,7 +52,7 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			chk_err = ft_print_conv(&ap, str, &i, 0);
+			chk_err = ft_print_conv(ap, str, &i, 0);
 			if (chk_err == -1)
 				return (-1);
 			else
